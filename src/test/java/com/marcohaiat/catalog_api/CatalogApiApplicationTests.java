4779f28b9826264489dc42bdc1d1efc2cade466e
Marco Haiat <marco.haiat@gmail.com>
2024-08-18 11:22:22 -0300

    feat: initial aplication structure

---
+++ src/test/java/com/marcohaiat/catalog_api/CatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcohaiat.catalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
