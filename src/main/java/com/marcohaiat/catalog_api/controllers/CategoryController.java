eb5c8e26c48dfbae5ccde11d76030c645b62d45f
Marco Haiat <marco.haiat@gmail.com>
2024-08-25 18:09:01 -0300

    fix: ownerId send on message

--- src/main/java/com/marcohaiat/catalog_api/controllers/CategoryController.java
+++ src/main/java/com/marcohaiat/catalog_api/controllers/CategoryController.java
@@ -3,9 +3,10 @@ package com.marcohaiat.catalog_api.controllers;
 import com.marcohaiat.catalog_api.domain.category.Category;
 import com.marcohaiat.catalog_api.domain.category.CategoryDTO;
 import com.marcohaiat.catalog_api.services.CategoryService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
@@ -21,5 +22,11 @@ public class CategoryController {
     public ResponseEntity<Category> insert(@RequestBody CategoryDTO categoryData) {
         Category newCategory = this.categoryService.insert(categoryData);
         return ResponseEntity.ok(newCategory);
+    }
+
+    @GetMapping("/by-owner/{id}")
+    public ResponseEntity<List<Category>> getCategoriesByOwner(@PathVariable("id") String ownerId) {
+        List<Category> allOwnerCategories = this.categoryService.getAllCategoriesByOwnerId2(ownerId);
+        return ResponseEntity.ok(allOwnerCategories);
     }
 }
--- src/main/java/com/marcohaiat/catalog_api/services/CategoryService.java
+++ src/main/java/com/marcohaiat/catalog_api/services/CategoryService.java
@@ -4,19 +4,27 @@ import com.marcohaiat.catalog_api.domain.category.Category;
 import com.marcohaiat.catalog_api.domain.category.CategoryDTO;
 import com.marcohaiat.catalog_api.domain.category.CategoryNotFound;
 import com.marcohaiat.catalog_api.reporitory.CategoryRepository;
-import com.marcohaiat.catalog_api.reporitory.OwnerRepository;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final OwnerService ownerService;
     private final SNSService snsService;
+    private final MongoTemplate mongoTemplate;
 
-    public CategoryService(CategoryRepository categoryRepository, SNSService snsService, OwnerService ownerService) {
+    public CategoryService(CategoryRepository categoryRepository, SNSService snsService, OwnerService ownerService, MongoTemplate mongoTemplate) {
         this.categoryRepository = categoryRepository;
         this.snsService = snsService;
         this.ownerService = ownerService;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public Category insert(CategoryDTO categoryData) {
@@ -24,11 +32,26 @@ public class CategoryService {
 
         Category newCategory = Category.toEntity(categoryData);
         this.categoryRepository.save(newCategory);
-        snsService.publish(newCategory.getId());
+        snsService.publish(newCategory.getOwnerId());
         return newCategory;
     }
 
     public void checkIfCategoryExists(String categoryId) {
         this.categoryRepository.findById(categoryId).orElseThrow(CategoryNotFound::new);
+    }
+
+    public List<Category> getAllCategoriesByOwnerId(String ownerId) {
+        return mongoTemplate.query(Category.class)
+                .matching(Query.query(where("ownerId").is(ownerId)))
+                .all();
+    }
+
+    public List<Category> getAllCategoriesByOwnerId2(String ownerId) {
+        return this.categoryRepository.getAllCategoriesByOwnerId2(ownerId);
+    }
+
+    public List<Category> getAllCategoriesByOwnerId4(String ownerId) {
+        Query query = new Query().addCriteria(Criteria.where("ownerId").is(ownerId));
+        return mongoTemplate.find(query, Category.class);
     }
 }
