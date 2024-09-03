674402b47821bf1e4b325dac2a44db4d80fffdbe
Marco Haiat <marco.haiat@gmail.com>
2024-09-03 12:05:55 -0300

    feat: spring security on/off switch implemented

--- src/main/java/com/marcohaiat/catalog_api/config/security/SecurityConfig.java
+++ src/main/java/com/marcohaiat/catalog_api/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.marcohaiat.catalog_api.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,15 +12,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Value("${spring.security.switch}")
+    private boolean isSecurityEnabled;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/api/owner").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
-                .build();
+        if (isSecurityEnabled) {
+            return httpSecurity
+                    .csrf(csrf -> csrf.disable())
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authorizeHttpRequests(authorize -> authorize
+                            .requestMatchers(HttpMethod.POST, "/api/owner").hasRole("ADMIN")
+                            .anyRequest().authenticated()
+                    )
+                    .build();
+        } else {
+            return httpSecurity
+                    .csrf(csrf -> csrf.disable())
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .build();
+        }
     }
 }
